9a506a758d7a628efac0605ce358699e311fd42e
jsundstrom1998 <jsundstrom1998@gmail.com>
2026-01-29 16:31:53 -0500

    Adding more to step0 script

--- bulk_processing/code/Step0_ERCC.sh
+++ bulk_processing/code/Step0_ERCC.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-## =========================
-## Define variables
-## =========================
+## Define variables 
 TEMP=$(getopt -o x:p:c:l:s:h \
   --long experiment:,prefix:,cores:,large:,stranded:,help \
   -n 'step0-ercc' -- "$@")
@@ -28,41 +26,86 @@ while true; do
     esac
 done
 
-## =========================
-## Setup
-## =========================
+## Setup 
 SOFTWARE=/dcs04/hicks/data/jsundstr/bulk_processing
-MAINDIR=$/dcs04/hicks/data/jsundstr/bulk_processing/fastq_test
-FILELIST=${MAINDIR}/samples.manifest
-
+MAINDIR=/dcs04/hicks/data/jsundstr/bulk_processing/fastq_test
 SHORT="ercc-${EXPERIMENT}"
-JOBNAME="step0-${SHORT}.${PREFIX}"
+JOBNAME="step0-${SHORT}.${PREFIX}" 
 
+FILELIST=${MAINDIR}/samples.manifest
 NUM=$(awk '{print $NF}' ${FILELIST} | uniq | wc -l)
+
 
 mkdir -p logs
 
-## Memory
-if [[ "${LARGE}" == "TRUE" ]]; then
-    MEM="10G"
+## Define memory
+if [[ $LARGE == "TRUE" ]]; then
+    MEM="10G"                    
 else
     MEM="5G"
 fi
 
-## Paired-end?
+## Paired-end flag 
 if [[ -f ".paired_end" ]]; then
     PE="TRUE"
 else
     PE="FALSE"
 fi
 
-## Strandedness
+## Strandedness flags
 STRANDOPTION=""
-case "${STRANDED}" in
-    FALSE) ;;
-    forward) STRANDOPTION="--fr-stranded" ;;
-    reverse) STRANDOPTION="--rf-stranded" ;;
-    *)
-        echo "Invalid stranded option: ${STRANDED}"
-        exit 1 ;;
-esac
+if [[ $STRANDED == "forward" ]]; then
+    STRANDOPTION="--fr-stranded"
+elif [[ $STRANDED == "reverse" ]]; then
+    STRANDOPTION="--rf-stranded"
+elif [[ $STRANDED != "FALSE" ]]; then
+    echo "Invalid stranded option"
+    exit 1
+fi
+
+## Write ERCC array job
+cat > .${JOBNAME}.slurm <<EOF
+#!/bin/bash
+#SBATCH --job-name=${JOBNAME}
+#SBATCH --cpus-per-task=${CORES}
+#SBATCH --mem=${MEM}
+#SBATCH --array=1-${NUM}%20
+#SBATCH --output=logs/${SHORT}.%a.out
+#SBATCH --error=logs/${SHORT}.%a.err
+
+echo "**** Job starts ****"
+date
+echo "Node: \$(hostname)"
+echo "Task ID: \$SLURM_ARRAY_TASK_ID"
+
+ID=\$(awk '{print \$NF}' ${FILELIST} | awk "NR==\$SLURM_ARRAY_TASK_ID")
+FILE1=\$(awk '{print \$1}' ${FILELIST} | awk "NR==\$SLURM_ARRAY_TASK_ID")
+
+if [[ "${PE}" == "TRUE" ]]; then
+    FILE2=\$(awk '{print \$3}' ${FILELIST} | awk "NR==\$SLURM_ARRAY_TASK_ID")
+fi
+
+mkdir -p ${MAINDIR}/Ercc/\${ID}
+
+if [[ "${PE}" == "TRUE" ]]; then
+    ${SOFTWARE}/kallisto quant \\
+        -i /dcs04/hicks/data/jsundstr/bulk_processing/ERCC/ERCC92.idx \\
+        -o ${MAINDIR}/Ercc/\${ID} \\
+        -t ${CORES} ${STRANDOPTION} \\
+        \${FILE1} \${FILE2}
+else
+    ${SOFTWARE}/kallisto quant \\
+        -i /dcs04/hicks/data/jsundstr/bulk_processing/ERCC/ERCC92.idx \\
+        -o ${MAINDIR}/Ercc/\${ID} \\
+        -t ${CORES} --single ${STRANDOPTION} \\
+        \${FILE1}
+fi
+echo "**** Job ends ****"
+date
+EOF
+
+## Submit ERCC job WITH dependency
+ERCC_JOBID=$(sbatch --dependency=afterok:${MERGE_JOBID} .${JOBNAME}.slurm | awk '{print $4}')
+
+echo "ERCC job submitted with JobID: ${ERCC_JOBID}"
+echo "Dependency: afterok:${MERGE_JOBID}"
