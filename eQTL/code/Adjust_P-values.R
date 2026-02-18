eaa56c9c7aee11e6ef2d90929c7b47597405d0d4
jsundstrom1998 <jsundstrom1998@gmail.com>
2026-02-18 14:06:30 -0500

    Scripts for adjusting p-values (added back from previously lost commit)

---
+++ eQTL/code/Adjust_P-values.R
@@ -0,0 +1,104 @@
+library("tidyverse")
+library("jaffelab")
+library("miniparquet")
+library("sessioninfo")
+library("here")
+library("qs2")
+library("getopt")
+
+data_directory = "/users/jsundstr/hicks_home/eQTL/"
+parquet_directory = "/dcs04/lieber/lcolladotor/pilotHb_LIBD001/Roche_Habenula/processed-data/17_eQTL/tensorQTL_output/"
+#pd_nominal = "/dcs04/lieber/lcolladotor/pilotHb_LIBD001/Roche_Habenula/processed-data/17_eQTL/tensorQTL_output/nominal"
+#pd_inter_thal = "/dcs04/lieber/lcolladotor/pilotHb_LIBD001/Roche_Habenula/processed-data/17_eQTL/tensorQTL_output/interaction_tot_Thal"
+#pd_inter_Hb = "/dcs04/lieber/lcolladotor/pilotHb_LIBD001/Roche_Habenula/processed-data/17_eQTL/tensorQTL_output/interaction_tot_Hb"
+
+#Read in which tensorQTL run mode is being used
+spec <- matrix(
+    c(
+        "mode", "m", 1, "character", "tensorQTL run mode",
+        "covariate", "c", 2, "character", "covariate applicable for interaction model"
+    ),
+    byrow = TRUE, ncol = 5
+)
+
+#Parse the command line arguments based on the specification
+opt <- getopt(spec)
+
+#Check if the provided mode is valid
+accepted_modes = c('nominal', 'cis', 'independent', 'interaction')
+if (!(opt$mode %in% accepted_modes)) {
+    stop(
+        sprintf(
+            "'opt$mode' must be in '%s'.",
+            paste(accepted_modes, collapse = "', '")
+        )
+    )
+}
+
+#Set output folder name suffix based on the mode and covariate (if interaction)
+if (opt$mode == "interaction") {
+    out_dir_suffix = sprintf('%s_%s', opt$mode, opt$covariate)
+} else {
+    out_dir_suffix = opt$mode
+}
+
+#Set output directory path
+out_dir = here(data_directory, "tensorQTL_output", out_dir_suffix)
+p_val_cutoff = 0.05
+
+#Retrieve the relevant parquet files based on the mode and read them in, adjusting the p-values accordingly
+if (opt$mode == 'nominal') {
+    parquet_files <- list.files(
+        parquet_directory,
+        pattern = "\\.parquet$",
+        full.names = TRUE
+    )
+
+    eqtl_out <- do.call("rbind", map(parquet_files, parquet_read)) |>
+        mutate(FDR = p.adjust(pval_nominal, "fdr"))
+} else if (opt$mode == "interaction") {
+    parquet_files <- list.files(
+        parquet_directory,
+        pattern = "\\.parquet$",
+        full.names = TRUE
+    )
+
+    eqtl_out <- do.call("rbind", map(parquet_files, parquet_read)) |>
+        mutate(FDR = p.adjust(pval_gi, "fdr"))
+    
+#
+    write_csv(eqtl_out, file.path(out_dir, 'all.csv'))
+} else if (opt$mode %in% c('cis', 'independent')) {
+    eqtl_out = read_csv(
+            file.path(out_dir, paste0(opt$mode, '_out.csv')),
+            show_col_types = FALSE
+        ) |>
+        mutate(FDR =  p.adjust(pval_beta, "fdr"))
+} else { 
+    stop(sprintf("Unsupported 'opt$mode' = '%s'.", opt$mode))
+}
+
+message("n pairs: ", nrow(eqtl_out))
+
+#Filter
+eqtl_out_filtered <- eqtl_out |>
+    filter(FDR < p_val_cutoff)
+message("n pairs FDR<", p_val_cutoff, ": ", nrow(eqtl_out_filtered))
+
+fn <- here(
+    out_dir,
+    paste0(
+        "FDR", as.character(p_val_cutoff) |> str_extract('\\.(.*)$', group = 1)
+    )
+)
+
+#Save as CSV and the faster qs
+write_csv(eqtl_out_filtered, file = paste0(fn, ".csv"))
+qs2::qsave(eqtl_out_filtered, file = paste0(fn, ".qs"))
+
+#Reproducibility information
+print("Reproducibility information:")
+Sys.time()
+proc.time()
+options(width = 120)
+session_info()
---
+++ eQTL/code/Adjust_P-values.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH -c 1
+#SBATCH --mem=20G
+#SBATCH --job-name=03_adj_p_vals
+#SBATCH -o /dev/null
+#SBATCH -e /dev/null
+
+run_mode=independent
+interaction_cov=none
+
+if [[ "$run_mode" == "interaction" ]]; then
+    log_path="logs/Adjust_P-value_${run_mode}_${interaction_cov}.log"
+else
+    log_path="logs/Adjust_P-value_${run_mode}.log"
+fi
+
+{
+set -e
+
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+
+module load conda_R/4.3
+Rscript Adjust_P-value.R -m $run_mode -c $interaction_cov
+
+echo "**** Job ends ****"
+date
+} > $log_path 2>&1
