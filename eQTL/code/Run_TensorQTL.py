fd689cae24bb1270cddcd23122852e0c7a8c921a
jsundstrom1998 <jsundstrom1998@gmail.com>
2026-03-05 14:40:42 -0500

    Finishing TensorQTL scripts and running TensorQTL (ignoring output files)

--- eQTL/code/Run_TensorQTL.py
+++ eQTL/code/Run_TensorQTL.py
@@ -31,7 +31,7 @@ else:
 #Specify paths and variables
 in_dir = Path(here(data_directory, "tensorQTL_input"))
 out_dir = Path(here(data_directory, "tensorQTL_output", out_dir_suffix))
-plink_prefix_path = "/dcs05/lieber/liebercentral/libdGenotype_LIBD001/BrainGenotyping/subsets/habenula_new/plink/habenula_maf05"
+plink_prefix_path = "/dcs04/hicks/data/data_proc/bulkDNAgenotype_habenula_human_pilot/habenula_maf05"
 covariates_file = str(in_dir / "covariates.txt")
 expression_bed = str(in_dir / "logcounts.bed.gz")
 
@@ -59,3 +59,94 @@ genotype_df = pr.load_genotypes().rename({'Br0983': 'Br983'}, axis = 1)
 variant_df = pr.bim.set_index('snp')[['chrom', 'pos']]
 print("Genotype dimensions:", end='')
 print(genotype_df.shape)
+
+genotype_ids = set(genotype_df.columns)
+covariates_ids = set(covariates_df.index)
+
+#   Exclude any samples that have genotypes but not covariates; halt in the
+#   reverse situation
+if (genotype_ids > covariates_ids):
+    extra_donors = list(genotype_ids - covariates_ids)
+    print("Excluding extra genotyped donors:", ','.join(extra_donors))
+    genotype_df.drop(extra_donors, axis = 1, inplace = True)
+elif (genotype_ids != covariates_ids):
+    print("Not all covariate samples in genotyped samples.")
+    sys.exit()
+
+## Fix chr names (maybe fix in plink?)
+if add_chr:
+    print("Adding 'chr' to genotype positions")
+    variant_df.chrom = [s.split(':')[0] for s in list(variant_df.index)]
+
+variant_chrom = set(variant_df.chrom)
+express_chrom = set(phenotype_pos_df.chr)
+
+#   Exclude any expressed chromosomes that aren't in genotyping data; halt in
+#   the reverse situation
+if express_chrom > variant_chrom:
+    print("Excluding phenotypes from these chromosomes:")
+    print(express_chrom - variant_chrom)
+    
+    chrom_filter = phenotype_pos_df.chr.isin(variant_chrom)
+    print("Phenotypes with chr in variants: " )
+    print(chrom_filter.value_counts())
+    
+    phenotype_df = phenotype_df[chrom_filter]
+    phenotype_pos_df = phenotype_pos_df[chrom_filter]
+elif express_chrom != variant_chrom:
+    print("Expression data must contain the same or a superset of chromosomes as the genotyping data.")
+    sys.exit()
+
+################################################################################
+#   Run tensorQTL
+################################################################################
+
+if run_mode == "nominal":
+    cis.map_nominal(
+        genotype_df, variant_df, phenotype_df, phenotype_pos_df,
+        prefix = prefix, covariates_df = covariates_df, maf_threshold = 0.05,
+        interaction_df = None, maf_threshold_interaction = 0, group_s = None,
+        window = 500000, run_eigenmt = True, output_dir = out_dir,
+        write_top = False, verbose = False
+    )
+elif run_mode == "cis":
+    cis_out = cis.map_cis(
+        genotype_df, variant_df, phenotype_df, phenotype_pos_df,
+        covariates_df = covariates_df, group_s = None, maf_threshold = 0.05,
+        beta_approx = True, nperm = 10000, window = 500000,
+        random_tiebreak = False, logger = None, seed = 118, verbose = True
+    )
+    cis_out.to_csv(out_dir / "cis_out.csv")
+elif run_mode == "independent":
+    cis_out = pd.read_csv(
+        out_dir.parent / "cis" / "cis_out.csv", index_col = 0
+    )
+
+    #   Compute q value, an expected input column
+    cis_out['qval'] = statsmodels.stats.multitest.fdrcorrection(cis_out['pval_beta'])[1]
+
+    ind_out = cis.map_independent(
+        genotype_df = genotype_df, variant_df = variant_df, cis_df = cis_out,
+        phenotype_df = phenotype_df, phenotype_pos_df = phenotype_pos_df,
+        covariates_df = covariates_df, group_s = None, maf_threshold = 0.05,
+        nperm = 10000, window = 500000, random_tiebreak = False, logger = None,
+        seed = 119, verbose = True
+    )
+
+    ind_out.to_csv(out_dir / "independent_out.csv")
+else:
+#'run_mode' must be 'interaction' based on check at the top of script
+    col_data = pd.read_csv(in_dir / "colData.csv", index_col = 'BrNum')
+    if interaction_cov not in col_data.columns:
+        print(f'Expected {interaction_cov} to be a valid colData column.')
+        sys.exit()
+
+    nominal_out = cis.map_nominal(
+        genotype_df, variant_df, phenotype_df, phenotype_pos_df,
+        prefix = prefix, covariates_df = covariates_df, output_dir = out_dir, 
+        interaction_df = col_data[[interaction_cov]],
+        maf_threshold_interaction = 0.05, group_s = None, window = 500000,
+        run_eigenmt = False, write_stats = True
+    )
+
+session_info.show()
---
+++ eQTL/code/Run_TensorQTL.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#SBATCH -c 1
+##SBATCH --gpus=1
+#SBATCH --mem=40G
+#SBATCH --job-name=Run_TensorQTL
+#SBATCH -o /dev/null
+#SBATCH -e /dev/null
+
+run_mode=independent
+interaction_cov=none
+
+if [[ "$run_mode" == "interaction" ]]; then
+    log_path="logs/run_tensorqtl_${run_mode}_${interaction_cov}.log"
+else
+    log_path="logs/run_tensorqtl_${run_mode}.log"
+fi
+
+{
+echo "**** Job starts ****"
+date
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${SLURM_JOB_ID}"
+echo "Job name: ${SLURM_JOB_NAME}"
+echo "Node name: ${SLURMD_NODENAME}"
+
+module load tensorqtl/1.0.8
+python Run_TensorQTL.py $run_mode $interaction_cov
+
+echo "**** Job ends ****"
+date
+} > $log_path 2>&1
